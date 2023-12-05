bc9ce4e751eb6f8444098aace6876aba54c6a1f8
Rong Huang <rongster3544@gmail.com>
2023-12-05 13:34:53 -0800

    day 5 part 1

--- day5.js
+++ day5.js
@@ -1,6 +1,34 @@
-import { fs } from 'fs';
+const fs = require('fs');
+const data = fs.readFileSync('input5.txt', 'utf8');
 
-var data = fs.readFileSync('input5.txt', 'utf8');
+let [seeds, ...blocks] = data.split('\r\n\r\n');
+seeds = seeds.split(':')[1].trim().split(' ').map(e=>Number(e));
+blocks = blocks.map(block => {
+    return block.trim().split('\r\n').slice(1).map(line=>{
+        return line.split(' ').map(e=>Number(e));
+    })
+});
 
-var arr = data.split('\n\n');
+console.log('seeds', seeds);
+console.log('blocks', blocks);
+
+// part 1
+
+for(let block of blocks){
+    seeds = seeds.map(seed => {
+        for(let line of block){
+            let [dest, src, range] = line;
+            if(seed>=src && seed<(src+range)){
+                return seed+dest-src;
+            }
+        }
+        return seed;
+    })
+}
+let min = Infinity;
+for(let seed of seeds){
+    min = Math.min(min, seed);
+}
+console.log('min', min);
+
 
