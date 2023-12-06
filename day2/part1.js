fbf45a3f2c7798fcddaaded6beb825e5f2ebb5e6
Rong Huang <rongster3544@gmail.com>
2023-12-06 15:24:51 -0800

    organize day2

--- day2/part1.js
+++ day2/part1.js
@@ -6,13 +6,20 @@ var games = data.trim().split('\n');
 let res = 0;
 for(let i=0;i<games.length;i++){
     let game = games[i].split(':')[1].trim().split('; ');
-    let amount = { red: 0, blue: 0, green: 0 };
+    let valid = true;
     for(let grab of game){
+        let amount = { red: 0, blue: 0, green: 0 };
         for(let group of grab.split(', ')){
             let [number, color] = group.split(' ');
             amount[color] = Math.max(amount[color], Number(number));
         }
+        if(amount['red'] > 12 || amount['green'] > 13 || amount['blue'] > 14){
+            valid = false;
+            break;
+        }
     }
-    res+= amount['red']*amount['blue']*amount['green'];
+    if(valid){
+        res+=i+1;
+    }
 }
 console.log('res', res);
